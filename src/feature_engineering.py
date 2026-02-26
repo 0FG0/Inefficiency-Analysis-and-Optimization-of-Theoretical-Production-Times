c4448c1cc98293c9e003d9a88c60485d8d9bbe61
Filippo Gentili <filgentili@gmail.com>
2026-02-26 12:44:14 +0100

    preparazione all'addestramento

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,3 +1,14 @@
+# ****** APPUNTI ******
+# Il feature engineering è il processo di trasformazione dei dati grezzi
+# in variabili (feature) che rendono gli algoritmi di machine learning più efficaci. 
+# I suoi obiettivi principali sono due: 
+# Rendere i dati compatibili: gli algoritmi non accettano dati testuali o date
+# dunque con il feature engineering trasformiamo i dati in formati leggibili dal modello.
+# Migliorare le prestazioni: aiuta il modello a cogliere sfumature e relazioni nascoste
+# nei dati che altrimenti passerebbero inosservate. 
+
+
+
 import pandas as pd
 
 # transforming time in numbers 
@@ -39,10 +50,17 @@ def add_deviation_feature(df):
     
     return df
 
+# how important was the inefficiency in the previous processing
+def add_lag_features(df, lag=1):
+    df = df.sort_values("Data_Ora_Fine")
+    df[f"lag_{lag}"] = df["Indice_Inefficienza"].shift(lag)
+    return df
+
 def feature_engineering_pipeline(df):
     df = add_time_features(df)
     df = add_rolling_features(df, window=10)
     df = add_deviation_feature(df)
+    df = add_lag_features(df)
     
     return df
 
